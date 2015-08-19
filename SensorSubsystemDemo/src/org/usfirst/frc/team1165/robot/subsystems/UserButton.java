178fa8fc32dc605330337026e86ae4bfe9cdca00
jkoehring <jkoehring@exponent.com>
2015-08-19 09:47:10 -0700

    First commit

---
+++ SensorSubsystemDemo/src/org/usfirst/frc/team1165/robot/subsystems/UserButton.java
@@ -0,0 +1,32 @@
+
+package org.usfirst.frc.team1165.robot.subsystems;
+
+import org.usfirst.frc.team1165.robot.commands.ReportUserButton;
+
+import edu.wpi.first.wpilibj.Utility;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class UserButton extends Subsystem
+{
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	public void initDefaultCommand()
+	{
+		// Set the default command for a subsystem here.
+		setDefaultCommand(new ReportUserButton());
+	}
+	
+	/**
+	 * Reports state of this subsystem on the Smart Dashboard.
+	 */
+	public void report()
+	{
+		SmartDashboard.putBoolean("User button", Utility.getUserButton());
+	}
+}
